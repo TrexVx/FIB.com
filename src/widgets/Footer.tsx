7711172dddf7b10a2de349764a88e5f79894a299
Panda <144740482+TrexVx@users.noreply.github.com>
2026-05-24 16:35:42 -0600

    Up: Modificado el Footer

--- src/widgets/Footer.tsx
+++ src/widgets/Footer.tsx
@@ -5,7 +5,12 @@ import './Footer.css'
 const Footer: React.FC = () => {
   return (
     <footer className="Footer">
-        <p>&copy; 2024 Federal Investigation Bureau. All rights reserved.</p>
+        <p>&copy; 2027 Federal Investigation Bureau. All rights reserved.</p>
+        <div>
+            <a href="#" className="social-link">Privacy Policy</a>
+            <a href="#" className="social-link">Terms of Service</a>
+            <a href="#" className="social-link">Contact Us</a>
+        </div>
     </footer>
   );
 }
